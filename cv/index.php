4a3d9d6c72cadde68dfde5e772da12f9d991fdd4
Ajay Brahmakshatriya <ajaybr@mit.edu>
2020-04-23 17:35:53 -0400

    Added CV update

---
+++ cv/index.php
@@ -0,0 +1,9 @@
+<?php include '../template.php'; ?>
+<?php include '../root-template.php'; ?>
+<script>document.getElementById("link-cv").classList.add("active");</script>
+
+<a href="/docs/CV_AjayBrahmakshatriya.pdf" class="icons-link" id="cv-download" download><img class="icons"  src="/../images/pdf.svg"/></a>
+<iframe src="main.html" onload="this.style.height=(this.contentWindow.document.body.scrollHeight)+'px';">
+</iframe>
+<?php include '../footer.php'; ?>
+
