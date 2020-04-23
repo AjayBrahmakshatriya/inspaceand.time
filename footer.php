dc07b5e38dd1517be4f39f86c1ccefbfbcf8426f
Ajay Brahmakshatriya <ajaybr@mit.edu>
2020-04-23 09:10:27 -0400

    Added empty page for the thoughts subdomain

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 
 </article>
 <div class="footer">
-<a href="/keys/ajaybrahmakshatriya.pgp.asc" title="B1A1 178A 773C 9D3A B524  E8CC F4E1 7EE4 BCA1 EA4E" download>0xBCA1EA4E (PGP)<a/>
+<a href="https://intimeand.space/keys/ajaybrahmakshatriya.pgp.asc" title="B1A1 178A 773C 9D3A B524  E8CC F4E1 7EE4 BCA1 EA4E" download>0xBCA1EA4E (PGP)<a/>
 </footer>
 </body>
 </html>
--- template.php
+++ template.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 <title>InTimeand.Space</title>
-<link rel="stylesheet" href="/css/style.css" />
-<script src="/js/main.js"> </script>
+<link rel="stylesheet" href="https://intimeand.space/css/style.css" />
+<script src="https://intimeand.space/js/main.js"> </script>
 
 </head>
 
@@ -15,7 +15,7 @@
 		<a href="https://wandering.intimeand.space/" id="wandering">wandering</a>	
 	</div>
 	<div class="links-right">
-		<a href="https://github.com/AjayBrahmakshatriya" target="_blank"><img src="/images/github.svg"/></a><a href="https://stackoverflow.com/users/2858773/ajay-brahmakshatriya" target="_blank"><img src="/images/stackoverflow.svg"></a><a href="https://www.linkedin.com/in/ajay-brahmakshatriya-868577a6/" target="_blank"><img src="/images/linkedin.svg"></a><a href="https://facebook.com/thats.me.ajay" target="_blank"><img src="/images/facebook.svg"/></a>
+		<a href="https://github.com/AjayBrahmakshatriya" target="_blank"><img src="https://intimeand.space/images/github.svg"/></a><a href="https://stackoverflow.com/users/2858773/ajay-brahmakshatriya" target="_blank"><img src="https://intimeand.space/images/stackoverflow.svg"></a><a href="https://www.linkedin.com/in/ajay-brahmakshatriya-868577a6/" target="_blank"><img src="https://intimeand.space/images/linkedin.svg"></a><a href="https://facebook.com/thats.me.ajay" target="_blank"><img src="https://intimeand.space/images/facebook.svg"/></a>
 	</div>
 
 	
