fe1a2e1be41c9662f550dfa1d488e54784e295fa
Ajay Brahmakshatriya <ajaybr@mit.edu>
2020-04-23 08:57:44 -0400

    Added htaccess and new icons

--- template.php
+++ template.php
@@ -4,20 +4,18 @@
 <title>InTimeand.Space</title>
 <link rel="stylesheet" href="/css/style.css" />
 <script src="/js/main.js"> </script>
+
 </head>
 
 <body>
 
 	<div class="links">
-		<a href="/" id="root">intimeand.space</a>
-		<a href="/thoughts" id="thoughts">thoughts</a>
-		<a href="/wandering" id="wandering">wandering</a>	
+		<a href="https://intimeand.space/" id="root">intimeand.space</a>
+		<a href="https://thoughts.intimeand.space/" id="thoughts">thoughts</a>
+		<a href="https://wandering.intimeand.space/" id="wandering">wandering</a>	
 	</div>
 	<div class="links-right">
-		<a href="https://github.com/AjayBrahmakshatriya" target="_blank"><img src="/images/github.svg"/></a>
-		<a href="https://stackoverflow.com/users/2858773/ajay-brahmakshatriya" target="_blank"><img src="/images/stackoverflow.png"></a>
-		<a href="https://www.linkedin.com/in/ajay-brahmakshatriya-868577a6/" target="_blank"><img src="/images/linkedin.png"></a>
-		<a href="https://facebook.com/thats.me.ajay" target="_blank"><img src="/images/facebook.png"/></a>
+		<a href="https://github.com/AjayBrahmakshatriya" target="_blank"><img src="/images/github.svg"/></a><a href="https://stackoverflow.com/users/2858773/ajay-brahmakshatriya" target="_blank"><img src="/images/stackoverflow.svg"></a><a href="https://www.linkedin.com/in/ajay-brahmakshatriya-868577a6/" target="_blank"><img src="/images/linkedin.svg"></a><a href="https://facebook.com/thats.me.ajay" target="_blank"><img src="/images/facebook.svg"/></a>
 	</div>
 
 	
